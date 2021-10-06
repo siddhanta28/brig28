c3fc79b25855e7ccaf5a254614122a3d243d559a
siddhanta tripathi <89008652+siddhanta28@users.noreply.github.com>
2021-10-06 17:06:14 +0530

    Update brig28.js

--- brig28.js
+++ brig28.js
@@ -4,3 +4,6 @@ const { events } = require("brigadier")
 events.on("exec", () => {
   console.log("==> handling an 'exec' event")
 })
+events.on("push", () => {
+  console.log(" **** I'm a GitHub 'push' handler")
+})
